70cc6d0eb9f4486a65e8e8ce314758a88f6b91f7
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2021-08-15 12:39:22 +0300

    jim

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+	var isMobile = {Android: function() {return navigator.userAgent.match(/Android/i);},BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},Windows: function() {return navigator.userAgent.match(/IEMobile/i);},any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}};
+if(isMobile.any()){}
+
+function ibg() {
+	$.each($('.ibg'), function(index, val) {
+		if($(this).find('img').length>0){
+			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+		}
+	});
+}
+ibg();
+
+// BurgerMenu
+  $('.icon-menu').click(function(){
+        $('.icon-menu, .menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
